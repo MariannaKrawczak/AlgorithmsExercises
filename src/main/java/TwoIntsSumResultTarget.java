5a3876389d9ff41deedecf92ef320cebcba43701
marianna <marianna>
2022-03-01 13:14:59 +0100

    Find the best time to buy and sell stock done

--- src/main/java/TwoIntsSumResultTarget.java
+++ src/main/java/TwoIntsSumResultTarget.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
 public class TwoIntsSumResultTarget {
   public int[] twoSum(int[] nums, int target) {
+
+    //HashMap
 
 //    int firstIndex = nums[0];
 
